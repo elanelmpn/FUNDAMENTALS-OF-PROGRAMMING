6f88e35c018d73e96cc575a3cbc1ea22d1976023
elanelmpn <2025-1-01380@lpu.edu.ph>
2025-09-12 09:36:08 +0800

    Act1.2.c

--- FPRL_LIMPIN/Act1.2.c
+++ FPRL_LIMPIN/Act1.2.c
@@ -1,32 +1,37 @@
 #include <stdio.h>
 
-int main() {
-    char name[50];
-    int age;
-    char course[50];
-    char school[50];
-    
-    printf("Enter your name: ");
+int main(){
+    char name[50], age[5], gender[20], birthday[50], address[100], No[12], email[50], course[50], year[50], hobby[50];
+
+    printf("Enter your Name: \n");
     scanf("%s", name);
-    
-    printf("Enter your age: ");
-    scanf("%d", age);
-    
-    printf("Enter your course: ");
+
+    printf("Enter your Age: \n");
+    scanf("%s", age);
+
+    printf("Enter your Gender: \n");
+    scanf("%s", gender);
+
+    printf("Enter your Birthday: \n");
+    scanf("%s", birthday);
+
+    printf("Enter your Address: \n");
+    scanf("%s", address);
+
+    printf("Enter your Contact No.: \n");
+    scanf("%s", No);
+
+    printf("Enter your Email: \n");
+    scanf("%s", email);
+
+    printf("Enter your course: \n");
     scanf("%s", course);
-    
-    printf("Enter your school: ");
-    scanf("%s", school);
-    
-    printf("\n-- user information --\n");
-    printf("Name: %s\n", name);
-    printf("Age: %d\n", age);
-    printf("Course: %s\n", course);
-    printf("School: %s\n", school);
-    printf("Status: Student\n");
-    printf("Goal: To Graduate\n");
-    printf("Hobby: Coding\n");
-    printf("Favorite Subject: Coding\n");
-    printf("Dream Job: IT\n");
+
+    printf("Enter your Year level: \n");
+    scanf("%s", year);
+
+    printf("Enter your Hobby: \n");
+    scanf("%s", hobby);
+
     return 0;
 }
